0a8138b5a50f8ebd92c201d00a1ae51500683040
Waleed Ehsan <waleed.ehsan@sky.uk>
2023-08-12 19:23:16 +0100

    Add DS Store to ignore and fix broken integration tests

--- src/main/resources/db/migration/V1__CREATE_INITIAL_SCHEMA.sql
+++ src/main/resources/db/migration/V1__CREATE_INITIAL_SCHEMA.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE users (
     id INT AUTO_INCREMENT primary key NOT NULL,
     first_name VARCHAR(256) NOT NULL,
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO user (id, first_name, last_name, email) VALUES (100, 'waleed', 'ehsan', 'waleed.ehsan@gmail.com');
-INSERT INTO user (id, first_name, last_name, email) VALUES (200, 'khaled', 'ehsan', 'khaled.ehsan@gmail.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (100, 'waleed', 'ehsan', 'waleed.ehsan@gmail.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (200, 'khaled', 'ehsan', 'khaled.ehsan@gmail.com');
